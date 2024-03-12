165051c2fc3b730bc3ef5fa6092531013558ba9c
Tomislav Dukez <tomdu3@ymail.com>
2024-03-12 17:26:22 +0000

    Add add tasks update on page

--- app/src/data/pocketbase.ts
+++ app/src/data/pocketbase.ts
@@ -1,8 +1,14 @@
 import PocketBase from 'pocketbase';
 
+console.log(
+    import.meta.env.POCKETBASE_URL ||
+      process.env.POCKETBASE_URL
+  )
+  
 export const pb = new PocketBase(
   import.meta.env.POCKETBASE_URL || process.env.POCKETBASE_URL
 );
+
 
 export async function getProjects() {
   const projects = await pb.collection('projects').getFullList();
@@ -35,4 +41,16 @@ export async function getProject(id: string) {
     })
   
     return newTask
+  }
+
+  export async function getTasks(project_id: string) {
+    const options = {
+      filter: `project = "${project_id}"`,
+    }
+  
+    const tasks = await pb
+      .collection('tasks')
+      .getFullList(options)
+  
+    return tasks
   }
