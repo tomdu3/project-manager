f3c3d3c4eec4a550dd483837c2926ff84de0b93d
Tomislav Dukez <tomdu3@ymail.com>
2024-03-12 12:08:22 +0000

    Add Get and Add project functions

--- app/src/data/pocketbase.ts
+++ app/src/data/pocketbase.ts
@@ -1,4 +1,20 @@
-import PocketBase from 'pocketbase'
+import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://127.0.0.1:8090')
+export const pb = new PocketBase(
+  import.meta.env.POCKETBASE_URL || process.env.POCKETBASE_URL
+);
 
+export async function getProjects() {
+  const projects = await pb.collection('projects').getFullList();
+
+  return projects;
+}
+
+export async function addProject(name: string) {
+  const newProject = await pb.collection('projects').create({
+    name,
+    status: 'not started',
+  });
+
+  return newProject;
+}
