5d63c15f8d83e27ef92def50b63f9a4655f2ddbe
Tomislav Dukez <tomdu3@ymail.com>
2024-07-02 10:29:55 +0200

    add database read and create new project

---
+++ app/src/data/pocketbase.ts
@@ -0,0 +1,22 @@
+import PocketBase from 'pocketbase'
+
+export const pb = new PocketBase(import.meta.env.POCKETBASE_URL ||
+    process.env.POCKETBASE_URL)
+
+export async function getProjects() {
+    const projects = await pb
+        .collection('projects')
+        .getFullList()
+    
+    return projects
+    }
+
+export async function addProject(name: string) {
+    const newProject = await pb.collection('projects')
+        .create({
+        name,
+        status: 'not started',
+        })
+        
+    return newProject
+    }
