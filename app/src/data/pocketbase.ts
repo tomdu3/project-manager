75f2fdf97f7cc9df676a273e4b1418b85eac8bc2
Tomislav Dukez <tomdu3@ymail.com>
2024-07-02 10:36:11 +0200

    add show single project details

--- app/src/data/pocketbase.ts
+++ app/src/data/pocketbase.ts
@@ -1,22 +1,27 @@
-import PocketBase from 'pocketbase'
+import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(import.meta.env.POCKETBASE_URL ||
-    process.env.POCKETBASE_URL)
+export const pb = new PocketBase(
+  import.meta.env.POCKETBASE_URL || process.env.POCKETBASE_URL
+);
 
 export async function getProjects() {
-    const projects = await pb
-        .collection('projects')
-        .getFullList()
-    
-    return projects
-    }
+  const projects = await pb.collection("projects").getFullList();
+
+  return projects;
+}
 
 export async function addProject(name: string) {
-    const newProject = await pb.collection('projects')
-        .create({
-        name,
-        status: 'not started',
-        })
-        
-    return newProject
-    }
+  const newProject = await pb.collection("projects").create({
+    name,
+    status: "not started",
+  });
+
+  return newProject;
+}
+
+export async function getProject(id: string) {
+  const project = await pb.collection("projects").getOne(id);
+
+  return project;
+}
+
