4f2c046e09af998403eefde28e693442ebf674c8
Tomislav Dukez <tomdu3@ymail.com>
2024-07-03 12:53:08 +0200

    add autocancellation of in pocketbase

--- app/src/data/pocketbase.ts
+++ app/src/data/pocketbase.ts
@@ -4,6 +4,9 @@ export const pb = new PocketBase(
   import.meta.env.POCKETBASE_URL || process.env.POCKETBASE_URL
 );
 
+// globally disable auto cancellation
+pb.autoCancellation(false)
+
 export async function getProjects() {
   const projects = await pb.collection("projects").getFullList();
 
