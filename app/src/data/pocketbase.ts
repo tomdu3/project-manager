a66ba6860828a3a730681e7ab0569cb49eb4104c
Tomislav Dukez <tomdu3@ymail.com>
2024-08-02 17:43:41 +0200

    add tasks record to pocketbase

--- app/src/data/pocketbase.ts
+++ app/src/data/pocketbase.ts
@@ -2,43 +2,42 @@ import PocketBase from "pocketbase";
 import type {
   TypedPocketBase,
   ProjectsResponse,
-  ProjectsRecord
-} from '@src/data/pocketbase-types'
+  ProjectsRecord,
+  TasksRecord,
+} from "@src/data/pocketbase-types";
 
 export const pb = new PocketBase(
   import.meta.env.POCKETBASE_URL || process.env.POCKETBASE_URL
 ) as TypedPocketBase;
 
 // globally disable auto cancellation
-pb.autoCancellation(false)
+pb.autoCancellation(false);
 
 function getStatus(project: ProjectsResponse) {
-	switch (project.status) {
-		case "not started":
-			return 7;
-		case "on hold":
-			return 6;
-		case "started":
-			return 5;
-		case "in progress":
-			return 4;
-		case "almost finished":
-			return 3;
-		case "ongoing":
-			return 2;
-		case "done":
-			return 1;
-		default:
-			return 0;
-	}
+  switch (project.status) {
+    case "not started":
+      return 7;
+    case "on hold":
+      return 6;
+    case "started":
+      return 5;
+    case "in progress":
+      return 4;
+    case "almost finished":
+      return 3;
+    case "ongoing":
+      return 2;
+    case "done":
+      return 1;
+    default:
+      return 0;
+  }
 }
 
 export async function getProjects() {
   const projects = await pb.collection("projects").getFullList();
 
-  return projects.sort(
-    (a, b) => getStatus(a) - getStatus(b)
-  );
+  return projects.sort((a, b) => getStatus(a) - getStatus(b));
 }
 
 export async function addProject(name: string) {
@@ -56,41 +55,37 @@ export async function getProject(id: string) {
   return project;
 }
 
-export async function addTask(
-  project_id: string,
-  text: string
-) {
-  const newTask = await pb.collection('tasks').create({
+export async function addTask(project_id: string, text: string) {
+  const newTask = await pb.collection("tasks").create({
     project: project_id,
     text,
-  })
+  });
 
-  return newTask
+  return newTask;
 }
 
 export async function getTasks(project_id: string) {
   const options = {
     filter: `project = "${project_id}"`,
-  }
+  };
 
-  const tasks = await pb
-    .collection('tasks')
-    .getFullList(options)
+  const tasks = await pb.collection("tasks").getFullList(options);
 
-  return tasks
+  return tasks;
 }
 
 export async function deleteProject(id: string) {
-  await pb.collection('projects').delete(id)
+  await pb.collection("projects").delete(id);
 }
 
-export async function updateProject(
-  id: string,
-  data: ProjectsRecord
-) {
-  await pb.collection('projects').update(id, data)
+export async function updateProject(id: string, data: ProjectsRecord) {
+  await pb.collection("projects").update(id, data);
 }
 
 export async function deleteTask(id: string) {
-  await pb.collection('tasks').delete(id)
+  await pb.collection("tasks").delete(id);
+}
+
+export async function updateTask(id: string, data: TasksRecord) {
+  await pb.collection("tasks").update(id, data);
 }
