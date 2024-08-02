33c0a13fdabbffe21205ccb0de2ccdb1d2168a7b
Tomislav Dukez <tomdu3@ymail.com>
2024-08-02 17:31:19 +0200

    add delete function to pocketbase

--- app/src/data/pocketbase.ts
+++ app/src/data/pocketbase.ts
@@ -90,3 +90,7 @@ export async function updateProject(
 ) {
   await pb.collection('projects').update(id, data)
 }
+
+export async function deleteTask(id: string) {
+  await pb.collection('tasks').delete(id)
+}
