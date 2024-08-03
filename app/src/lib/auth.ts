6dc6b8c3d4f1f056f09b712adb73e8195a145cc0
Tomislav Dukez <tomdu3@ymail.com>
2024-08-03 13:27:07 +0200

    add create user and validations

---
+++ app/src/lib/auth.ts
@@ -0,0 +1,42 @@
+import { pb } from '@src/data/pocketbase'
+
+export const isValidEmail = (email: string) => {
+    if (typeof email !== 'string') return false
+    if (email.length > 255) return false
+    const regex = /^.+@.+$/
+    return regex.test(email)
+  }
+  
+  export const isValidPassword = (password: string) => {
+    if (typeof password !== 'string') return false
+    if (password.length > 255) return false
+    if (password.length < 4) return false
+    return true
+  }
+  
+  export function isValidData(
+    email: string,
+    password: string
+  ) {
+    if (!isValidEmail(email)) {
+      return false
+    }
+  
+    if (!isValidPassword(password)) {
+      return false
+    }
+  
+    return true
+  }
+
+  export async function createUser(
+    email: string,
+    password: string
+  ) {
+    return await pb.collection('users').create({
+      email: email,
+      password: password,
+      passwordConfirm: password,
+      emailVisibility: true,
+    })
+  }
